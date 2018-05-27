a6a94af43c8597d4a6ba12cca9ce10c3eeb8fb87
Marcus Dicander <dicander@kth.se>
2018-05-27 16:25:52 +0200

    Improved variable names.

--- bytegame.py
+++ bytegame.py
@@ -15,26 +15,28 @@ def main():
         "O": "O (letter)",
         "l": "l (letter)",
     }
-    for x in answers:
+    for number in answers:
         before = time.time()
-        to_ask = chr(x)
+        to_ask = chr(number)
         if to_ask in confusing.keys():
             to_ask = confusing[to_ask]
         guess = input("Correct answers : " + str(correct) + "\n " + to_ask + " : ")
-        if guess.isdigit() and int(guess) == x:
+        if guess.isdigit() and int(guess) == number:
             print("Yes.")
             correct += 1
-            speeds.append((time.time() - before, chr(x)))
+            speeds.append((time.time() - before, chr(number)))
         else:
-            mistakes.append(to_ask)
-        print(x, "was the right answer.")
+            mistakes.append(to_ask + " was " + str(number) + " not " + guess + ".")
+        print(number, "was the right answer.")
     print(correct, "right answers out of", len(answers))
     tot_time = time.time() - time_before
     print("Total time: {:.0f}min {:.2f}s.".format(tot_time / 60, tot_time % 60))
-    print("Your mistakes: ", ", ".join([chr(x) for x in mistakes]))
+    print("Your mistakes:")
+    for current in mistakes:
+        print(current)
     print("Your results, sorted from slowest.")
-    for x in reversed(sorted(speeds)):
-        print("{:.2f}s : {:s}".format(x[0], x[1]))
+    for element in reversed(sorted(speeds)):
+        print("{:.2f}s : {:s}".format(element[0], element[1]))
 
 
 if __name__ == '__main__':
