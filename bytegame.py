9687dd1c4a0779193e0c40931630973a0a3ae921
Marcus Dicander <dicander@kth.se>
2018-09-19 10:59:31 +0200

    Refactored the game into three functions instead of one whale sized function

--- bytegame.py
+++ bytegame.py
@@ -3,19 +3,24 @@ import time
 
 
 def main():
-    answers = list(range(32,127))
-    random.shuffle(answers)
-    correct = 0
-    mistakes = []
-    speeds = []
-    time_before = time.time()
-    confusing = {
-        "0": "0 (digit)",
-        "1": "1 (digit)",
-        "O": "O (letter)",
-        "l": "l (letter)",
-    }
-    for number in answers:
+    """This program runs a game that asks the user for ascii codes"""
+    answers, confusing, correct, mistakes, speeds, time_before = \
+        generate_questions()
+    correct = run_game(answers, confusing, correct, mistakes, speeds)
+    print(correct, "right answers out of", len(answers))
+    tot_time = time.time() - time_before
+    print("Total time: {:.0f}min {:.2f}s.".format(tot_time / 60, tot_time % 60))
+    print("Your mistakes:")
+    for current in mistakes:
+        print(current)
+    print("Your results, sorted from slowest.")
+    for element in reversed(sorted(speeds)):
+        print("{:.2f}s : {:s}".format(element[0], element[1]))
+
+
+def run_game(answers, confusing, correct, mistakes, speeds):
+    """Ask the questions, return number of answers. Modifies mistakes and speeds."""
+    for number in answers[:10]:
         before = time.time()
         to_ask = chr(number)
         if to_ask in confusing.keys():
@@ -28,15 +33,24 @@ def main():
         else:
             mistakes.append(to_ask + " was " + str(number) + " not " + guess + ".")
         print(number, "was the right answer.")
-    print(correct, "right answers out of", len(answers))
-    tot_time = time.time() - time_before
-    print("Total time: {:.0f}min {:.2f}s.".format(tot_time / 60, tot_time % 60))
-    print("Your mistakes:")
-    for current in mistakes:
-        print(current)
-    print("Your results, sorted from slowest.")
-    for element in reversed(sorted(speeds)):
-        print("{:.2f}s : {:s}".format(element[0], element[1]))
+    return correct
+
+
+def generate_questions():
+    """Generates all data related to a game."""
+    answers = list(range(32, 127))
+    random.shuffle(answers)
+    correct = 0
+    mistakes = []
+    speeds = []
+    time_before = time.time()
+    confusing = {
+        "0": "0 (digit)",
+        "1": "1 (digit)",
+        "O": "O (letter)",
+        "l": "l (letter)",
+    }
+    return answers, confusing, correct, mistakes, speeds, time_before
 
 
 if __name__ == '__main__':
